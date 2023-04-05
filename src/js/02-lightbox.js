23aa354fd11c5b3d8a2e3f6f1457db99ea3c8b42
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-04-05 22:55:54 +0200

    task2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+let galleryEl = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+  galleryEl.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+  );
+}
+
+galleryEl.addEventListener("click", (event) => event.preventDefault());
+
+var lightbox = new SimpleLightbox(".gallery li a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  showCounter: true,
+  scrollbarWidth: 20,
+});
